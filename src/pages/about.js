c390506e44f4ba854e09088e79ea6c642512c813
Dani Pardo <danielp.5@hotmail.com>
2022-07-28 14:18:07 +0200

    push

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,18 +10,24 @@ function About(){
     return(<>
         <div className='primaryNav'><text></text><div className="footergran left"><a href="https://github.com/Pardo24" target="_blank" rel="noreferrer"><GitHubIcon  style={{padding:"15px 10px 0 15px", color:'black'}}/></a> <a href="https://www.linkedin.com/in/daniel-pardo-celaya-53a999242/" target="_blank" rel="noreferrer"><LinkedInIcon style={{padding:"15px 30px 0 15px", color:'black'}}/></a>
         </div> <MenuComp  size='large' style={{marginRight:'20px'}}/></div>
-    <div className="about"style={{marginTop:'10%',padding:'10px 0'}}>
+    <div className="about"style={{marginTop:'5%',padding:'10px 0'}}>
         <img src={about} alt='water' width={500} height={400}/>
-    <div className="aboutContent" style={{textAlign:'center'}}>
-        <h1>Why?</h1>
+    <div className="aboutContent" style={{lineHeight:'25px'}}>
+        <h1 style={{textAlign:'left!important'}}>Why?</h1>
         <br/>
-        <p>We live in a world where a lot of water is wasted,<br/><br/> 
+        <p><b>The Water Map és una eina de salut pública al servei del ciutadà</b><br/>
+        S’ajunta en un sol mapa serveis moltes vegades desconeguts: fonts d’aigua potable, WC públics i piscines. <br/>
+        Amb aquesta app construim un pont de comunicació bidireccional entre la institució i la ciutadania. <br/>
+        Es posa en valor el coneixement dels serveis que ofereix la ciutat per millorar la vida del seus habitants. <br/>
+        Disfrutem al màxim de la nostra ciutat!<br/><br/> 
+</p>
 
-        the natural resources are running out, <br/><br/>
-        and every year the weather is getting hotter. <br/><br/>
-        <br/><br/>
-        The aim of this project is to take advantage of the resources our cities bring <br/><br/>
-        to reduce the waste of one of our most precious resource; <br/><br/><b style={{color:'#1F4690'}}>the Water</b></p>
+<p><b>The Water Map is a public health tool at the service of the citizen</b><br/>
+        It brings together on a single map services often unknown: drinking water sources, public toilets and swimming pools. <br/>
+        With this app we build a two-way communication bridge between the institution and the citizens.<br/>
+        It enhances the knowledge of the services offered by the city to improve the lives of its inhabitants.<br/>
+        Let's enjoy our city to the fullest!</p>
+
        </div> </div></>
     )
 }
