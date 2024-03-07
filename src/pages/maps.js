56ce3d56b66bedc0a939e9e20d307542dee04484
Daniel Pardo Celaya <danielp.5@hotmail.com>
2024-03-07 18:24:23 +0100

    fix(map): tile layer not accesible anymore

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -183,14 +183,6 @@ function Maps() {
             ></TileLayer>
           </BaseLayer>
 
-          <BaseLayer name="Dark">
-            <TileLayer
-              url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-              maxZoom="20"
-              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-            ></TileLayer>
-          </BaseLayer>
-
           <BaseLayer name="Satellite View">
             <TileLayer
               url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
